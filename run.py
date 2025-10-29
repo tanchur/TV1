e6a095c1b2b90fe124627470dd49f4f4f78ca15f
tanchur <tkanchyyr@mail.ru>
2025-10-29 22:28:33 +0700

    my commit

--- run.py
+++ run.py
@@ -1,14 +1,13 @@
 import sys
 import os
-import tkinter as tk
 
 # Добавляем текущую директорию в путь Python
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from app.main import MortgageApp
+import tkinter as tk
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = MortgageApp(root)
     root.mainloop()
-
